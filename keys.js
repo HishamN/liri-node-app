6c6f0238290e8a0ae35d1acd65a9467e3da15be0
Hisham Noman <nourmarket@Hishams-MacBook-Pro.local>
2018-05-21 02:12:07 -0400

    working good

---
+++ keys.js
@@ -0,0 +1,8 @@
+// console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'iNz8uJUxPN996Ko8AaCDlvGgY',
+  consumer_secret: '64EvR4o7HphxCxn7so9FNjdSdhkkE1TJDlQvKu53OM1WklkM2B',
+  access_token_key: '987388736936792067-8qDZSmpTYD7hIOPHGTgjQaAGzyTzpnw',
+  access_token_secret: 'vPxCJR1VsmFqY3Inpj4EBwdABcNokfQJrfq5yvB1uq4tj',
+}
